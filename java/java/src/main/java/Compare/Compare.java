9bd4cd2cab25ea24ee63b6ad1cb61e08f54dbc2a
fkdl3010 <fkdl3919@icloud.com>
2021-11-09 00:10:45 +0900

    /java-and-spring << Feat: Comparable 코드 추가

--- java/java/src/main/java/Compare/Compare.java
+++ java/java/src/main/java/Compare/Compare.java
@@ -1,6 +1,8 @@
 package Compare;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +11,10 @@ import java.util.stream.Stream;
 public class Compare {
 
     public static void main(String[] args) {
-        GilGamer gilGamer1 = new GilGamer(10);
-        GilGamer gilGamer2 = new GilGamer(20);
-        GilGamer gilGamer3 = new GilGamer(30);
-        GilGamer gilGamer4 = new GilGamer(40);
+        GilGamer gilGamer1 = new GilGamer(20);
+        GilGamer gilGamer2 = new GilGamer(30);
+        GilGamer gilGamer3 = new GilGamer(40);
+        GilGamer gilGamer4 = new GilGamer(10);
         GilGamer gilGamer5 = new GilGamer(50);
 
         List<GilGamer> gilGamerList = new ArrayList<GilGamer>();
@@ -24,13 +26,15 @@ public class Compare {
 
         List<Integer> collect = gilGamerList.stream().flatMap(x -> Stream.of(x.getScore())).collect(Collectors.toList());
 
-        System.out.println(collect);
+        System.out.println("base = " + collect);
 
         List<Integer> collect1 = collect.stream().sorted().collect(Collectors.toList());
 
-        System.out.println(collect1);
+        System.out.println("stream sorted = " + collect1);
 
-        
+        Collections.sort(gilGamerList);
+
+        System.out.println("컴페러블의 compareTo 사용 시 = " + gilGamerList);
 
     }
 }
--- java/java/src/main/java/Compare/GilGamer.java
+++ java/java/src/main/java/Compare/GilGamer.java
@@ -1,13 +1,29 @@
 package Compare;
 
 import lombok.Getter;
+import optional.GilOptional;
 
 @Getter
-public class GilGamer {
+public class GilGamer implements Comparable<GilGamer>{
 
     int score;
 
+
     public GilGamer(int score) {
         this.score = score;
+    }
+
+    @Override
+    public int compareTo(GilGamer o) {
+        System.out.println("!!");
+        return o.score - score; // 내림차순 desc
+//        return score - o.score; // 오름차순 asc
+    }
+
+    @Override
+    public String toString() {
+        return "GilGamer{" +
+            "score=" + score +
+            '}';
     }
 }
